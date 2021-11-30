56341c2ab2dfdd3f1e487123eb4bf35f6d838e2a
takazawa1204 <ckicken1204@gmail.com>
2021-11-30 07:15:50 +0000

    good

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about' => 'homes#about'
   resources :users, only: [:show,:index,:edit,:update]
-  resources :books, only: [:create,:show,:index,:edit,:update,:destroy,]
+  resources :books, only: [:create,:show,:index,:edit,:update,:destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
