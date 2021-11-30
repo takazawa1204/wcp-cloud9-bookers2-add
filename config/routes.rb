74ebc7267a31e9f6b7bb92851d846747c01d8028
takazawa1204 <ckicken1204@gmail.com>
2021-11-30 08:23:49 +0000

    Task3

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show,:index,:edit,:update]
   resources :books, only: [:create,:show,:index,:edit,:update,:destroy] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
 end
