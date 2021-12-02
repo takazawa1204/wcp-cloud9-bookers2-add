905e2a8aa3e95caccfee667f29e997d2b1b56249
takazawa1204 <ckicken1204@gmail.com>
2021-12-02 06:44:37 +0000

    Task5

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'home/about' => 'homes#about'
+  get '/search', to: 'searchs#search'
   resources :users, only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
