6c2982f8c0c2dc26a81091330822e2e71b37302b
takazawa1204 <ckicken1204@gmail.com>
2021-12-05 13:56:45 +0000

    Task7a

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,9 @@
 class Book < ApplicationRecord
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
+	has_many :favorites_users, through: :favorites, source: :user
 	has_many :book_comments, dependent: :destroy
-	
+
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
